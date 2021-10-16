44afd3d8610c16291c3c304bf04efd0f01968f7a
Yash254 <72601812+Yash254@users.noreply.github.com>
2021-10-16 19:45:33 +0530

    Add files via upload

---
+++ Cloud.py
@@ -0,0 +1,23 @@
+import dropbox
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+
+        f=open(file_from,'rb')
+        dbx.files_upload(f.read(), file_to)
+
+def main():
+    access_token = 'sl.A6fd0IjqucAluURuVJz7wh0L1gY_Cp8j6gF5UX_R3jkGXpzKOrh7eqCfmY6YHxVcPqUqiJS3FWKtJXG0sRzhhlgFBPjUpgvp756BU7buyebWLGr9QlcDGh5cqmwdpg-L24RI542BXXA'
+    transferData = TransferData(access_token)
+
+    file_from =input("enter the file to be transfered")     
+    file_to = input("enter the full path to upload to dropbox")
+
+    transferData.upload_file(file_from, file_to)
+    print("file has transfered")
+
+main()
